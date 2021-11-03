871ce82ec4e74d3ec9f2dc3f89cb147270d69ef9
Raymond Ariwoola <Raymondariwoola@gmail.com>
2021-11-03 14:17:36 +0400

    minor fixes

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ import nodemailer from 'nodemailer';
 const app = express();
 const dbUsername = process.env.DB_USERNAME;
 const dbPassword = process.env.DB_PASSWORD;
+const siteURL = "https://www.example.com/";
 
 app.set('view engine', 'ejs');
 
@@ -30,23 +31,6 @@ const transporter = nodemailer.createTransport({
     debug: false,
     logger: true
 });
-
-function sendEmail(userEmail) {
-    const mailOptions = {
-        from: 'raymondariwoola@gmail.com',
-        to: userEmail,
-        subject: 'Your reservation is confirmed',
-        html: emailDocument
-    };
-
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Email sent: ' + info.response);
-        }
-    });
-}
 
 
 // Connect to Mongo DB
@@ -75,6 +59,7 @@ function randomString(length, chars) {
 var reservationData = {
     "foodItems": ""
 };
+
 
 // home route
 app.get("/", function (req, res) {
@@ -115,17 +100,85 @@ app.post("/reservation", function (req, res) {
     });
 
     newReservationnewReservation.save();
-    // sendEmail(userEmail); // send email
+    sendEmail(reservationData.emailAddress); // send email
     res.redirect("/completed"); // redirect user to the completed page
 });
 
 
 
+function sendEmail(userEmail) {
+    const context = reservationData;
 
-const emailDocument = `
+    const mailOptions = {
+        from: 'raymondariwoola@gmail.com',
+        to: userEmail,
+        subject: 'Your reservation is confirmed',
+        html: ''
+    };
 
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+}
+const emailDocument = `<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css" />
+<style>
+	* {
+		box-sizing: border-box;
+	}
 
-`;
+	body {
+		margin: 0;
+		padding: 0;
+	}
+
+	h1 {
+		padding: 30px 20px;
+		text-decoration: underline;
+	}
+
+	.container {
+		max-width: 500px;
+		    padding: 20px;
+	}
+</style>
+
+<div class="container">
+	<h1 style="font-size:30px;"><strong>Your reservation is confirmed</strong></h1>
+	<br>
+	<span style="font-size:22px;">Ray's Kitchen</span>
+	<br>
+	<p style="font-size:15px;">Table for 2 on Saturday, July 18, 2020 at 7:00 pm</p>
+	<br>
+	<p style="margin: 0; font-size: 14px; text-align: left ;">
+		<strong><span style="font-size:15px;">Name:</span></strong>
+	</p>
+	<br>
+	<p style="margin: 0; font-size: 14px; text-align: left;">
+		<strong><span style="font-size:15px;">Confirmation # :</span></strong>
+	</p>
+	<br>
+
+	<div style="padding-top:15px;padding-bottom:20px;">
+		<div style="font-family: Arial, sans-serif">
+			<div
+				style="font-size: 12px; font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif; mso-line-height-alt: 18px; color: #555555; line-height: 1.5;">
+				<p style="margin: 0; font-size: 14px; text-align: center; mso-line-height-alt: 22.5px;">
+					<span style="font-size:15px;">Apartment number 407,</span>
+				</p>
+				<p style="margin: 0; font-size: 14px; text-align: center; mso-line-height-alt: 22.5px;">
+					<span style="font-size:15px;">City Tower Block B1.</span>
+				</p>
+				<p style="margin: 0; font-size: 14px; text-align: center; mso-line-height-alt: 22.5px;">
+					<span style="font-size:15px;">Ajman, UAE.</span>
+				</p>
+			</div>
+		</div>
+	</div>
+</div>`;
 
 
 
