406b6bd6345132fe7b096fbcd23bd4f3bb6c6b21
Raymond Ariwoola <Raymondariwoola@gmail.com>
2021-11-05 01:50:08 +0400

    change sendgrid SMTP to API

--- app.js
+++ app.js
@@ -8,12 +8,15 @@ import _ from 'lodash';
 import nodemailer from 'nodemailer';
 import {reservationEmail, reservationEmailFull} from './emailTemplate.js';
 import moment from 'moment';
+import { createRequire } from "module";
 
+const require = createRequire(import.meta.url);
+const sgMail = require('@sendgrid/mail');
 
 const app = express();
 const dbUsername = process.env.DB_USERNAME;
 const dbPassword = process.env.DB_PASSWORD;
-const siteURL = "https://www.example.com/";
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 app.set('view engine', 'ejs');
 
@@ -21,19 +24,6 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(express.static("public"));
-
-const transporter = nodemailer.createTransport({
-    host: 'smtp.sendgrid.net',
-    port: 465,
-    service: 'sendgrid',
-    secure: false,
-    auth: {
-        user: process.env.MAILUSERNAME,
-        pass: process.env.MAILPASSWORD
-    },
-    debug: false,
-    logger: true
-});
 
 
 // Connect to Mongo DB
@@ -120,9 +110,7 @@ app.post("/reservation", function (req, res) {
 });
 
 
-
 function sendEmail(userEmail) {
-    const registerUser = async (req, res) => {
         const {
             customerName,
             reservationNumber,
@@ -134,23 +122,24 @@ function sendEmail(userEmail) {
 
         const output = reservationEmailFull(customerName, reservationNumber, persons, date, time, emailAddress);
 
-        let mailOptions = {
-            from: 'raymondariwoola@gmail.com',
-            to: userEmail,
-            subject: 'Your reservation is confirmed!',
-            text: 'Hello World',
-            html: output,
-        }
-
-        transporter.sendMail(mailOptions, function (error, info) {
-            if (error) {
-                console.log(error);
-            } else {
-                console.log('Email sent: ' + info.response);
-            }
-        });
-    }
+      const msg = {
+          to: userEmail,
+          from: 'ariwoolao@raymondui.com',
+          subject: 'Your reservation is confirmed',
+          text: '',
+          html: output,
+      }
+      sgMail
+          .send(msg)
+          .then(() => {
+              console.log('Email sent' + msg)
+          })
+          .catch((error) => {
+              console.error(error)
+          })
 }
+
+
 
 
 // Listening ports
